57e8dca104c7c0e07b0b75771a8473c14051f4c0
Anton Harkushyn (Anton293) <werior36@gmail.com>
2026-01-17 05:42:35 +0200

    update path prefix

--- backend/src/api/v1/__init__.py
+++ backend/src/api/v1/__init__.py
@@ -6,4 +6,4 @@ from fastapi import APIRouter
 from src.api.v1.endpoints import example
 
 api_router = APIRouter()
-api_router.include_router(example.router, prefix="/", tags=["base"])
+api_router.include_router(example.router, prefix="/base", tags=["base"])
